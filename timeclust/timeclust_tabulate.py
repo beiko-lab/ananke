6879a6b8301a8d875194adf0b44cbe68d9739163
mwhall <hallm2533@gmail.com>
2016-02-23 13:52:49 -0400

    Add checks for inconsistent data Currently just complains about it

--- timeclust/timeclust_tabulate.py
+++ timeclust/timeclust_tabulate.py
@@ -25,6 +25,8 @@ def aggregate(sequence_path, metadata_path, time_name, timeseriesdata_path, outs
     #Add metadata to HDF5 file
     seqcount = defaultdict(lambda: defaultdict(int))
     prev_sample_name = ""
+    #Keep track of skipped sequences
+    skipped = 0
     for line in seqf:
         #Assume first line is header
         sample_name = line.split("_")[0][1:]
@@ -34,13 +36,16 @@ def aggregate(sequence_path, metadata_path, time_name, timeseriesdata_path, outs
             else:
                 #Skip the next sequence
                 seqf.next()
-                i+=1
+                i += 1
+                skipped += 1
                 continue
         sequence = seqf.next().strip()
         seqcount[sequence][sample_name] += 1
         i+=1
         if (i%10000 == 0):
             print(i)
+    if (skipped > 0):
+        print("WARNING: Skipped %d sequences (no match to sample name in metadata file)" % (skipped,))
     print("Writing table to file")
     ngenes = len(seqcount)
     nsamples = len(sample_name_array)
@@ -53,6 +58,8 @@ def aggregate(sequence_path, metadata_path, time_name, timeseriesdata_path, outs
     data = []
     indptr = []
     indices = []
+    #Use to ensure all indices exist at the end
+    unique_indices = []
     hashseq_list = []
     j = 0
     for sequence, abundance_dict in seqcount.iteritems():
@@ -67,6 +74,8 @@ def aggregate(sequence_path, metadata_path, time_name, timeseriesdata_path, outs
                 rowsum += abundance
                 data.append(abundance)
                 indices.append(col)
+                if (col not in unique_indices):
+                    unique_indices.append(col)
                 j += 1
                 
         outseqf.write(">%s;size=%d;\n" % (hashseq,rowsum))
@@ -81,4 +90,6 @@ def aggregate(sequence_path, metadata_path, time_name, timeseriesdata_path, outs
             
     timeseriesdb.add_timeseries_data(data, indices, indptr, hashseq_list)
     print("Done writing to %s"%timeseriesdata_path)
-    #del timeseriesdb
+    #Check consistency of the data
+    if (len(unique_indices) < nsamples):
+        print("WARNING: Number of time-points retrieved from sequence file is less than the number of samples in metadata file. %d samples are missing. This is probably not what you want." % (nsamples - len(unique_col),))
