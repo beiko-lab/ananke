eaae040ed51d1a1b9053b94ce4ebfe2692814b42
mwhall <hallm2533@gmail.com>
2018-05-31 10:46:55 -0300

    Implementing a more robust solution to storing bloom filters
    HDF5 does not like binary data, so we convert the bitarray to a uint8array and back

--- ananke/_cluster.py
+++ ananke/_cluster.py
@@ -3,7 +3,7 @@ from ananke._database import AnankeDB
 from ananke._DBloomSCAN import DBloomSCAN, ExternalHashBloom
 from bitarray import bitarray
 import numpy as np
-from zlib import compress
+from zlib import compress, decompress
 
 def find_nearest_timeseries(anankedb, query_data, tsdist, n_threads = 1, n_chunks = 100, in_memory=True):
     if in_memory:
@@ -36,35 +36,61 @@ def find_nearest_timeseries(anankedb, query_data, tsdist, n_threads = 1, n_chunk
                 min_index = k
     return min_index
 
+def bitarray_to_uint8array(barray):
+    chunks = np.arange(len(barray), 0, -8)
+    uint8array=np.zeros(len(chunks), dtype='uint8')
+    for i, u in enumerate(chunks):
+        l = max(0,u-8)
+        chunk = barray[l:u]
+        chunk = chunk[::-1]
+        uint8array[i] = int(chunk.to01(), 2)
+    return uint8array
+
+def uint8array_to_bitarray(uint8array, length):
+    bits = list(np.unpackbits(uint8array)[length - 1::-1])
+    return bitarray(bits)
+
 def save_blooms(anankedb, dbloomscan):
     bfs = anankedb._h5t.require_group("bloomfilters")
     attrs = bfs.attrs
-    for bloom in dbloomscan.bloom_garden.blooms:
-        barray = np.void(compress(dbloomscan.bloom_garden.blooms[bloom].bitarray.tobytes()))
-        attrs[str(bloom)] = barray
-
-def load_blooms(anankedb, distance_measure="sts", data_in_memory=True):
-    if distance_measure == "sts":
-        distance_function = sts_distance
-    elif distance_measure == "dtw":
-        distance_function = dtw_distance
-    elif distance_measure == "ddtw":
-        distance_function = ddtw_distance
-    else:
-        raise ValueError("Unknown distance measure '%s'." % (distance_measure,))
+    attrs["max_dist"] = dbloomscan.max_dist
+    for bloom_id in dbloomscan.bloom_garden.blooms:
+        bloom = dbloomscan.bloom_garden.blooms[bloom_id]
+        uint8array = bitarray_to_uint8array(bloom.bitarray)
+        bloom_size = len(bloom.bitarray)
+        if str(bloom_id) in bfs:
+            bfs[str(bloom_id)].write_direct(uint8array)
+            attrs[str(bloom_id)] = bloom_size
+        else:
+            bfs.create_dataset(str(bloom_id), 
+                               (len(uint8array),), 
+                               dtype="uint8",
+                               data = uint8array)
+            attrs[str(bloom_id)] = bloom_size
+    #Clean up any extra data sets
+    for bloom_id in bfs:
+        #If it isn't from this import
+        if float(bloom_id) not in dbloomscan.bloom_garden.blooms.keys():
+            del bfs[bloom_id]
+        
+def load_blooms(anankedb, distance_measure="sts", in_memory=True):
+    time_points = anankedb.get_timepoints()
+    dist = distance_function(distance_measure, time_points=time_points)
     if "bloomfilters" not in anankedb._h5t:
         raise IndexError("No bloom filters to load.")
-    capacity = anankedb._h5t["timeseries/ids"].shape[0]
     bfs = anankedb._h5t["bloomfilters"]
     attrs = bfs.attrs
-    data_fetcher = data_fetcher_factory(anankedb, data_in_memory)
-    dist_range = anankedb._h5["bloomfilters/dist_range"][:]
-    dbloomscan = DBloomSCAN(capacity, distance_function,
-                            data_fetcher, dist_range)
-    for bloom_array in attrs:
-        new_bloom = ExternalHashBloom(capacity)
-        new_bloom.bitarray = bitarray(bloom_array)
-        bloom_garden.blooms[bloom] = new_bloom
+    data_fetcher = data_fetcher_factory(anankedb, dist, in_memory)
+    dist_range = sorted([float(bloom_id) for bloom_id in anankedb._h5t["bloomfilters"]])
+    dbloomscan = DBloomSCAN(anankedb.nts, dist.distance,
+                            data_fetcher, dist_range, max_dist = attrs["max_dist"])
+    for bloom_id in bfs:
+        bloom_size = attrs[bloom_id]
+        new_bloom = ExternalHashBloom(20*anankedb.nts)
+        uint8array = bfs[bloom_id][:]
+        barray = uint8array_to_bitarray(uint8array, bloom_size)
+        new_bloom.bitarray = barray
+        dbloomscan.bloom_garden.blooms[float(bloom_id)] = new_bloom
     return dbloomscan
 
 def data_fetcher_factory(anankedb, tsdist, in_memory=True):
